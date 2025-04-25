08e9f199bbed904f7769108c431e26295e3c1b11
mkg26 <92150824+MKG26@users.noreply.github.com>
2025-04-25 17:07:24 +0530

    interceptor service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,36 @@ import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
 import { OrdersComponent } from './orders/orders.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, HeaderComponent, HomeComponent, ProductsComponent, CartComponent, OrdersComponent],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    OrdersComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
